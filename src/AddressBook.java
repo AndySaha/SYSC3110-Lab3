5d7557aed3ef4d9b95d7856453e883360a0c762c
AndySaha <andrew.sahadeo2002@gmail.com>
2022-09-19 14:20:30 -0400

    This is a second change to test

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -17,6 +17,10 @@ public class AddressBook {
 
     public static void main(String[] args) {
         System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Erik","6134206666","123 Street Ln");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 
 }
